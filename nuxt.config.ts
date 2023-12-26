545a74b56eb7bad37159d2b830f7dc3afce65d3c
RusKayskas <KayskasRus@yandex.ru>
2023-12-26 23:44:33 +0300

    add plugins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  modules: [
+  '@nuxtjs/tailwindcss', 
+  'shadcn-nuxt', 
+  '@nuxtjs/supabase'],
+  shadcn: {
+    prefix: 'Ui',
+    componentDir: './components/ui'
+  },
+  supabase: {
+    redirectOptions: {
+      login: '/auth',
+      callback: '/confirm',
+      exclude: ['/'],
+    }
+  }
 })
---
+++ pages/auth.vue
@@ -0,0 +1,22 @@
+<template>
+  <Auth
+    :supabaseClient="supabaseClient"
+    :appearance="{
+      theme: ThemeSupa
+    }"
+    redirectTo="/"
+    :providers="['google']"
+  />
+</template>
+
+<script setup lang="ts">
+  // Import predefined theme
+  import { ThemeSupa } from '@supabase/auth-ui-shared';
+  import { Auth } from '@nuxtbase/auth-ui-vue';
+
+  const supabaseClient = useSupabaseClient();
+  
+  definePageMeta({
+    layouts: 'lAuth'
+  });
+</script>
