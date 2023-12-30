d4849f0a52ec1048df9333b1b9abc6b54ab9242f
RusKayskas <KayskasRus@yandex.ru>
2023-12-30 16:13:34 +0300

    add sidebar navigation

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,17 +1,16 @@
 <template>
   <aside class="space-y-4 w-full py-4 flex-col flex h-full bg-white shadiw-sm border-r">
     <div class="px-3 py-2 flex-1">
-      <NuxtLink class=" flex items-center pl-3 mb-14" to="/">
+      <NuxtLink class=" flex items-center pl-3 mb-10" to="/">
         <div class="relative h-8 w-8 mr-4">
           <NuxtImg style="height: 100%" src="/logo.svg" />
         </div>
         <h1 class="text-2xl font-bold">AiGenX</h1>
       </NuxtLink>
-      <div class="space-y-1">
-        <NuxtLink 
-          v-for="route in DASHBORDLINKS" 
-          :to="route.link" 
-          :key="route.name" 
+      <ul class="space-y-1">
+        <li v-for="route in DASHBORDLINKS" :key="route.name">
+          <NuxtLink 
+          :to="route.link"  
           class="text-sm group text-primary flex p-3 justify-start font-medium cursor-pointer rounded-lg transition hover:bg-muted"
           :class="{ 'bg-muted': bgMuted(route.link) }"
           >
@@ -19,8 +18,9 @@
             <Icon :name="route.icon" :class="`h-5 w-5 mr-3 ${route.color}`" />
             <span>{{ route.name }}</span>
           </div>
-        </NuxtLink>
-      </div>
+         </NuxtLink>
+        </li>
+      </ul>
     </div>
   </aside>
 </template>
